c6431f75bbb76df40ef99a468465974ae1783ccd
Chuck <you@example.com>
2020-11-11 14:53:24 -0800

    refactor to be more modular

--- live-code/live-app-example/src/App.js
+++ live-code/live-app-example/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 const TurtleUser = ({ 
@@ -15,67 +16,79 @@ const TurtleUser = ({
     </div>
 );
 
-function App() {
-  const turtleData = [
-    {
-      name: 'Gamera',
-      shellRadius: '40 meters',
-      favoriteFood: 'lava',
-      environment: 'sky',
-      country: 'japan'
-    },
-    {
-      name: 'Leonardo',
-      shellRadius: '1.5 meters',
-      favoriteFood: 'pizza',
-      environment: 'urban',
-      country: 'USA'
-    },
-    {
-      name: 'Leonardo',
-      shellRadius: '1.5 meters',
-      favoriteFood: 'pizza',
-      environment: 'urban',
-      country: 'USA'
-    },
-    {
-      name: 'Crush',
-      shellRadius: '0.5 meters',
-      favoriteFood: 'sea food',
-      environment: 'ocean',
-      country: 'Aus'
-    },
-  ];
+class TurtleList extends React.Component {
+  render() {
+    const turtleData = [
+      {
+        name: 'Gamera',
+        shellRadius: '40 meters',
+        favoriteFood: 'lava',
+        environment: 'sky',
+        country: 'japan'
+      },
+      {
+        name: 'Leonardo',
+        shellRadius: '1.5 meters',
+        favoriteFood: 'pizza',
+        environment: 'urban',
+        country: 'USA'
+      },
+      {
+        name: 'Leonardo',
+        shellRadius: '1.5 meters',
+        favoriteFood: 'pizza',
+        environment: 'urban',
+        country: 'USA'
+      },
+      {
+        name: 'Crush',
+        shellRadius: '0.5 meters',
+        favoriteFood: 'sea food',
+        environment: 'ocean',
+        country: 'Aus'
+      },
+    ];
+    // save space with a jsx spread
+    const turtleComponents = turtleData.map(turtle => (<TurtleUser {...turtle} />));
+    return (
+      <section>
+        {turtleComponents}
+      </section>
+    );
+  }
+}
 
-  const turtleComponents = turtleData.map(turtle => (
-    <TurtleUser
-      name={turtle.name}
-      shellRadius={turtle.shellRadius}
-      favoriteFood={turtle.favorite}
-      environment={turtle.environment}
-      country={turtle.country}
-    />
-  ))
-  return (
+const Header = () => (
+  <header>
+    <h1>Giant monster turtles! Meet other giant monster turtles</h1>
+  </header>
+)
+
+const Footer = () => (
+  <footer>
     <div>
-      <header>
-        <h1>Giant monster turtles! Meet other giant monster turtles</h1>
-      </header>
-      <main>
-        <h2>Current turtles near you!</h2>
-        <section>
-          {turtleComponents}
-        </section>    
-      </main>
-      <footer>
-        <div>
-          Contact info
-        </div>
-        <div>
-          Address
-        </div>
-      </footer>
+      Contact info
+   </div>
+    <div>
+      Address
     </div>
+  </footer>
+);
+
+const MainContent = () => (
+  <main>
+    <h2>Current turtles near you!</h2>
+    <TurtleList />
+  </main>
+);
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </>
   );
 }
 
